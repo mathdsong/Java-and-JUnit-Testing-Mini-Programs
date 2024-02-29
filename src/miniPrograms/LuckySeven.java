7988fdd23a62bd7e9473a4f8b5c0787d968d6266
dongningsong <mathdsong@gmail.com>
2024-02-29 02:26:54 -0700

    input validation

--- src/miniPrograms/LuckySeven.java
+++ src/miniPrograms/LuckySeven.java
@@ -4,11 +4,24 @@ import java.util.Scanner;
 public class LuckySeven {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("How much you would like to bet?");
-        int initialBet = sc.nextInt();
+        int initialBet = 0;
+
+        while (initialBet > 999999 || initialBet < 1) {
+            System.out.println("How much you would like to bet?");
+            while (!sc.hasNextInt()) {
+                System.out.println("invalid literal for int() with base 10: " + sc.next());
+                System.out.println("How much you would like to bet?");
+            }
+            initialBet = sc.nextInt();
+            if (initialBet > 999999) {
+                System.out.println("Bet too much!");
+            } else if (initialBet < 1) {
+                System.out.println("Bet too little!");
+            }
+        }
+
         int maxReward = 0, totalRolls = 0;
-        double avgRolls = (double) 0;
-        for (int i = 1; i < 11; i++) {
+        for (int i = 1; i < 21; i++) {
           System.out.println("Game " + i + " :");
           int rollsPlayed = 0;
           int currReward = initialBet, currMax = initialBet;
@@ -19,17 +32,16 @@ public class LuckySeven {
               if (dice1 + dice2 == 7) {
                   currReward += 4;
                   currMax = Math.max(currMax, currReward);
+                  maxReward = Math.max(maxReward, currMax);
               } else {
                   currReward -= 1;
               }
-              maxReward = Math.max(maxReward, currMax);
           }
           System.out.println("Number of rolls played: " + rollsPlayed);
           System.out.println("Max player money: " + currMax);
           totalRolls += rollsPlayed;
         }
-        avgRolls = (double) totalRolls / 10;
-        System.out.println("Average rolls per simulation: " + avgRolls);
+        System.out.println("Average rolls per simulation: " + (double) totalRolls / 20);
         System.out.println("Most money player won: " + maxReward);
     }
 }
