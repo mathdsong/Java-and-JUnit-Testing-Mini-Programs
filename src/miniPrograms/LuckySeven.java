9d7d603e6b8d283883e072cbc5a4b6ca7d2cf81b
dongningsong <mathdsong@gmail.com>
2024-03-01 18:34:34 -0700

    works for decimal input

--- src/miniPrograms/LuckySeven.java
+++ src/miniPrograms/LuckySeven.java
@@ -7,31 +7,31 @@ public class LuckySeven {
         Scanner sc = new Scanner(System.in);
         var initialBet = "0";
 
-        while (Integer.parseInt(initialBet) > 999999 || Integer.parseInt(initialBet)  < 1) {
+        while (Double.parseDouble(initialBet) > 999999 || Double.parseDouble(initialBet)  < 1) {
             // Prompt the user to input a bet:
             System.out.print("How much you would like to bet?");
             initialBet = sc.nextLine();
-            // if the bet entered by user is not an integer or if it's a blank input, will ask user to re-enter a new bet
-            while (!initialBet.matches("^[+-]?[0-9]*") || initialBet.matches("^\\s*$")) {
+            // if the bet entered by user is not a decimal (including negative) or if it's a blank input, will ask user to re-enter a new bet
+            while (!initialBet.matches("^-?\\d*\\.?\\d*$") || initialBet.matches("^\\s*$")) {
                 System.out.println("invalid literal for int() with base 10: '" + initialBet + "'");
                 System.out.print("How much you would like to bet?");
                 initialBet = sc.nextLine();
             }
             // control the bet range:
-            if (Integer.parseInt(initialBet)  > 999999) {
+            if (Double.parseDouble(initialBet)  > 999999) {
                 System.out.println("Bet too much!");
-            } else if (Integer.parseInt(initialBet)  < 1) {
+            } else if (Double.parseDouble(initialBet)  < 1) {
                 System.out.println("Bet too little!");
             }
         }
 
         sc.close();
 
-        int maxReward = 0, totalRolls = 0;
+        double maxReward = 0, totalRolls = 0;
         for (int i = 1; i < 101; i++) {
             System.out.println("Game " + i + " :");
             int rollsPlayed = 0;
-            int currReward = Integer.parseInt(initialBet) , currMax = Integer.parseInt(initialBet) ;
+            double currReward = Double.parseDouble(initialBet) , currMax = Double.parseDouble(initialBet) ;
             Random r = new Random();
             while (currReward >= 0) {
                 rollsPlayed++;
