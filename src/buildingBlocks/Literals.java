2c1665fdf46f337c4ba1824fb5b9ed98f0844228
dongningsong <mathdsong@gmail.com>
2024-03-18 16:36:43 -0600

    literals

---
+++ src/buildingBlocks/Literals.java
@@ -0,0 +1,10 @@
+package buildingBlocks;
+
+public class Literals {
+    static long max = 2147483649L;
+    static double uglyButLegal = 1_235_6.8_9;
+    public static void main(String[] args) {
+        System.out.println(max);
+        System.out.println(uglyButLegal);
+    }
+}
